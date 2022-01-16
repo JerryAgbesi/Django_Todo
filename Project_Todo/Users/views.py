d96bdcba2ebaad68a419b46affb08d42ed7aa851
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-01-16 14:45:20 -0800

    testing success message

--- Project_Todo/Users/views.py
+++ Project_Todo/Users/views.py
@@ -1,17 +1,23 @@
+from email import message
+
 from django.shortcuts import render
 from .forms import Registrationform
-
+from django.contrib.messages import constants as messages
 # Create your views here.
 def register(request):
     form = Registrationform
     if request == 'POST':
-        form = Registrationform
+        form = Registrationform(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.SUCCESS(request,f'account created for {username}')
+        else :
+            messages.ERROR(request,'error creating account')    
         
 
     context = {'form':form}        
-    return(render(request,'Users/register.html',context))
+    return(render(request,'Users/register.html',context,))
 
 def login(request):
     return (render(request,'Users/login.html'))
