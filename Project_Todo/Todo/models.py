a8e61a6e07c425f0f46aabd5b326b9d58a9d1387
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-01-17 21:09:05 -0800

    tried to create separate pages for users

--- Project_Todo/Todo/models.py
+++ Project_Todo/Todo/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
     date_added = models.DateTimeField()
     task = models.CharField(max_length=200)
+
+class TodoItem(models.Model):
+    content = models.TextField()
+    currentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.content)
--- Project_Todo/Todo/views.py
+++ Project_Todo/Todo/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from django.utils import timezone
 from Todo.models import Todo
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from .models import TodoItem
 # from django import 
 
 # Create your views here.
@@ -13,14 +15,27 @@ def home(request):
     tasks = Todo.objects.all().order_by('-date_added')
     return(render(request,'Todo/base.html',{'todo_items':tasks}))
 
+@login_required(login_url='login')
 def add_todo(request):
     current_date = timezone.now()
     content = request.POST['content']
 
-    Todo.objects.create(date_added = current_date, task= content )
+    if content:
+        new_item = TodoItem(content=content,currentUser = request.user)
+        Todo.objects.create(date_added = current_date, task= content ,currentUser=request.user)
 
     return HttpResponseRedirect('/')
 
+@login_required(login_url='login')
 def delete_todo(request,todo_id):
     Todo.objects.get(id=todo_id).delete()
-    return HttpResponseRedirect('/')    
+    return HttpResponseRedirect('/') 
+
+
+class ItemListView(ListView):
+    model = TodoItem
+    template_name = "Todo/base.html" 
+    context_object_name = "todo_items"
+
+    def get_queryset(self):
+        return TodoItem.objects.filter(currentUser=self.request.user)       
