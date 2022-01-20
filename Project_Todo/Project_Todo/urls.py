d091b6e4381efe65934e5e160e1e55c40df3d22e
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-01-20 14:01:54 -0800

    registered todo items on the admin page

--- Project_Todo/Project_Todo/urls.py
+++ Project_Todo/Project_Todo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as deView
-from accounts import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Project_Todo/Todo/models.py
+++ Project_Todo/Todo/models.py
@@ -4,14 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Todo(models.Model):
     current_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_added = models.DateTimeField()
+    date_added = models.DateTimeField('Date Added')
     task = models.CharField(max_length=200)
 
     def __str__(self):
         return str(self.task)
 
-# class TodoItem(models.Model):
-#     content = models.TextField()
-#     currentUser = models.ForeignKey(User, on_delete=models.CASCADE)
 
    
